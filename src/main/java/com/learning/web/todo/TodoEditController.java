8ea85b837162beecaa67764c5b8f5925b47e234d
Danil Ober <ober2013@mail.ru>
2023-12-07 20:24:14 +0700

    add: todo edit

---
+++ src/main/java/com/learning/web/todo/TodoEditController.java
@@ -0,0 +1,67 @@
+package com.learning.web.todo;
+
+import com.learning.dao.TodoDao;
+import com.learning.dao.UserDao;
+import com.learning.entity.Todo;
+import com.learning.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by ulyanov on 30.09.16.
+ */
+@Controller
+public class TodoEditController {
+    @Autowired private TodoDao todoDao;
+    @Autowired private TodoEditValidator todoEditValidator;
+
+    @RequestMapping(value = "/todo/todoEdit.html", method = RequestMethod.GET)
+    public String onGet(
+            Model model,
+            @RequestParam(required = false) Integer todoId
+    ) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user;
+        if (principal instanceof User)
+            user = (User)principal;
+        else
+            return "redirect:/login.html";
+
+        Todo command = todoDao.getTodoById(todoId);
+        if (command==null){
+            command = new Todo();
+            command.setUserId(user.getUserId());
+        }
+        model.addAttribute("command", command);
+        return "todo/todoEdit";
+    }
+
+    @RequestMapping(value = "/todo/todoEdit.html", method = RequestMethod.POST, params = "!_cancel")
+    public String onPost(
+            Model model,
+            @ModelAttribute("command") Todo command,
+            BindingResult result
+    ) {
+        todoEditValidator.validate(command, result);
+        if (!result.hasErrors()) {
+            todoDao.saveOrUpdate(command);
+        } else {
+            model.addAttribute("errors", result.getAllErrors());
+            model.addAttribute("command", command);
+            return "todo/todoEdit";
+        }
+        return "redirect:/todo/todos.html?todoId=" + command.getTodoId();
+    }
+
+    @RequestMapping(value = "/todo/todoEdit.html", method = RequestMethod.POST, params = "_cancel")
+    public String onCancel() {
+        return "redirect:/todo/todos.html";
+    }
+}
---
+++ src/main/java/com/learning/web/todo/TodoEditValidator.java
@@ -0,0 +1,49 @@
+package com.learning.web.todo;
+
+import com.learning.dao.TodoDao;
+import com.learning.dao.UserDao;
+import com.learning.entity.Todo;
+import com.learning.entity.User;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+/**
+ * Created by ulyanov on 30.09.16.
+ */
+@Repository("todoEditValidator")
+public class TodoEditValidator implements Validator {
+    @Autowired private TodoDao todoDao;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Todo.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User)authentication.getPrincipal();
+        if (user == null) {
+            errors.reject("user", "Нельзя создать задачу анонимному пользователю!");
+            return;
+        }
+
+        Todo todo = (Todo)o;
+        if (StringUtils.isBlank(todo.getName())) {
+            errors.reject("name", "Нужно указать задачу, она не должна быть пустой!");
+            return;
+        }
+        if (todo.getName().length() < 3) {
+            errors.reject("name", "Содержание задачи должно быть как минимум длиною в 3 символа!");
+            return;
+        }
+
+        if (!user.isAdmin() & user.getUserId() != todo.getUserId())
+            errors.reject("user", "Нет доступа на редактирование чужих записей!");
+    }
+}
